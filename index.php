3cdcf5116ace89df0d7037a35f2e0e20479da8af
Nicolas Carrillo <nick@ncnewmedia.com>
2025-01-02 00:30:47 +0000

    g
    d

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("header.php"); ?>
+<!-- page content -->
+
+
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8 col-md-6 bg-grey"> 
+                    <!-- <div class="masthead-subheading">Welcome To Our Studio!</div> -->
+                    <div class="masthead-heading text-uppercase">upcycled & handmade goods</div>
+                    <!--    <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a> -->
+                    </div>
+                    <div class="col-sm-4 col-md-6"></div>
+                </div>
+            </div>
+        </header>
+        <!-- home-->
+        <section class="page-section" id="home">
+
+        <div class="container">
+        
+            <h4 class="subheading">Handmade in Cheshire, England</h4>
+        
+            <p class="copy-text">
+            Saved&made use discarded and surplus materials to create rugged practical items and equipment for 
+            every adventure.
+            </p>
+
+            <p class="copy-text">
+            The materials are mostly sourced from Army surplus as well as from used tents, truck tarpaulins, and 
+            deconstructed army surplus. I try to preserve original features where possible, preserving authenticity 
+            and adding to the identity of each item. In some products, you might see the outline of an old pocket, 
+            the evidence of something removed, or even a secret pocket!
+            </p>
+        </div>
+        </section>
+      
+  
+<?php include("footer.php"); ?>
